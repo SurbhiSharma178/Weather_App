93c1b89751f52a259d910356b5493f9e4188ec3e
SurbhiSharma178 <surbhisharma20040416@gmail.com>
2024-08-03 20:21:09 +0530

    Modified

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import React from 'react'
+import TodayData from './components/TodayData'
+import OtherDays from './components/OtherDays'
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="outer-box">
+    <TodayData/>
+    <OtherDays/>
     </div>
-  );
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/TodayData.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './weather.css';
+
+const TodayData = () => {
+  return (
+    <>
+    <div className="upper-box">
+      <div className="nav">
+        <button>Ajmer</button>
+        <button> Jaipur</button>
+        <button>Udaipur</button>
+        <button>Jodhpur</button>
+        <button>Bikanar</button>
+      </div>
+      <input type="text" placeholder='Search by city'/>
+
+      </div>
+
+    </>
+  )
+}
+
+export default TodayData
